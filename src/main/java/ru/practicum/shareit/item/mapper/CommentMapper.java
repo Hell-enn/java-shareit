9c296834479d9688fd32d5f2e078f7fb40080c0e
ElenaKlimkina <klimkina201485@gmail.com>
2024-06-23 22:09:08 +0700

    fix: delete unused import

--- src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
